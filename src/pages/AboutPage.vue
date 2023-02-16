0c198828d74edfa71fa081dc24e0479194722ef7
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-02-16 14:39:35 +0530

    Pages & Routes
    Created "About" page and added routes for the same.
    Changed the Main header to display page title dynamically.
    https://quasar.dev/vue-components/list-and-list-items#qitem-api

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="">
+    <h5>About</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AboutPage",
+});
+</script>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="">
+    <h5>Home</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomePage",
+});
+</script>
