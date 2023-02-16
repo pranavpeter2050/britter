1bd53d3b0fc6d3d8d65cf3f6222c0de3b50a349d
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-02-16 16:45:19 +0530

    Tweet List - Design for single tweet-card
    https://quasar.dev/vue-components/list-and-list-items#more-involved-examples
    https://youtu.be/la-0ulfn0_M?t=3603

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -33,6 +33,38 @@
     </div>
 
     <q-separator class="divider" size="10px" color="grey-2" />
+
+    <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar size="lg">
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <strong>Annie</strong>
+            <span class="text-grey-7"> @annie_og</span></q-item-label
+          >
+          <q-item-label class="tweet-content text-body1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enimad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat.
+          </q-item-label>
+
+          <div class="tweet-icons row justify-between q-mt-sm">
+            <q-btn flat round color="grey" size="sm" icon="far fa-comment" />
+            <q-btn flat round color="grey" size="sm" icon="fas fa-retweet" />
+            <q-btn flat round color="grey" size="sm" icon="far fa-heart" />
+            <q-btn flat round color="grey" size="sm" icon="fas fa-trash" />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top> 1 min ago </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -59,4 +91,10 @@ export default defineComponent({
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-4
+
+.tweet-content
+  white-space: pre-line
+
+.tweet-icons
+  margin-left: -5px
 </style>
