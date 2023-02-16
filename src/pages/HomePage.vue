e568812e4246aee39c8600b7f617525600f6168a
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-02-16 15:18:58 +0530

    New Tweet form
    https://quasar.dev/vue-components/input#standard
    https://quasar.dev/vue-components/button
    https://quasar.dev/layout/grid/row

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,34 @@
 <template>
   <q-page class="">
-    <h5>Home</h5>
+    <div class="q-py-lg q-px-md">
+      <q-input
+        class="new-tweet"
+        bottom-slots
+        v-model="newTweetContent"
+        placeholder="What's happening?"
+        counter
+        maxlength="280"
+        :dense="dense"
+        autogrow
+      >
+        <template v-slot:before>
+          <q-avatar size="lg">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+          </q-avatar>
+        </template>
+
+        <template v-slot:after>
+          <q-btn
+            :disabled="!newTweetContent"
+            unelevated
+            rounded
+            no-caps
+            color="primary"
+            label="Tweet"
+          />
+        </template>
+      </q-input>
+    </div>
   </q-page>
 </template>
 
@@ -9,5 +37,17 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "HomePage",
+  data() {
+    return {
+      newTweetContent: "",
+    };
+  },
 });
 </script>
+
+<style lang="sass">
+.new-tweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+</style>
