7abef948d8e4bed8ac4850534adb37e875fad59b
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-02-16 15:32:21 +0530

    New tweet input field style refinement
    https://quasar.dev/layout/grid/row
    https://quasar.dev/layout/grid/introduction-to-flexbox
    https://quasar.dev/layout/grid/gutter
    https://quasar.dev/vue-components/separator
    https://quasar.dev/vue-components/separator#qseparator-api

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,34 +1,38 @@
 <template>
   <q-page class="">
-    <div class="q-py-lg q-px-md">
-      <q-input
-        class="new-tweet"
-        bottom-slots
-        v-model="newTweetContent"
-        placeholder="What's happening?"
-        counter
-        maxlength="280"
-        :dense="dense"
-        autogrow
-      >
-        <template v-slot:before>
-          <q-avatar size="lg">
-            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
-          </q-avatar>
-        </template>
-
-        <template v-slot:after>
-          <q-btn
-            :disabled="!newTweetContent"
-            unelevated
-            rounded
-            no-caps
-            color="primary"
-            label="Tweet"
-          />
-        </template>
-      </q-input>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          class="new-tweet"
+          bottom-slots
+          v-model="newTweetContent"
+          placeholder="What's happening?"
+          counter
+          maxlength="280"
+          :dense="dense"
+          autogrow
+        >
+          <template v-slot:before>
+            <q-avatar size="lg">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          class="q-mb-lg"
+          :disabled="!newTweetContent"
+          unelevated
+          rounded
+          no-caps
+          color="primary"
+          label="Tweet"
+        />
+      </div>
     </div>
+
+    <q-separator class="divider" size="10px" color="grey-2" />
   </q-page>
 </template>
 
@@ -50,4 +54,9 @@ export default defineComponent({
   textarea
     font-size: 19px
     line-height: 1.4 !important
+
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
 </style>
