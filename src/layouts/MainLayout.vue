e7336ee276f582bb38ed8c570f79975a2372e282
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-02-16 11:17:27 +0530

    Left drawer (navigation)
    Enabled "Fontawesome" in quasar.config.js, added "bird" icon.
    https://quasar.dev/vue-components/icon
    https://quasar.dev/style/spacing
    https://quasar.dev/vue-components/list-and-list-items
    https://quasar.dev/style/typography
    https://quasar.dev/layout/drawer#qdrawer-api

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,8 +8,34 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+    <q-drawer
+      :width="283"
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+    >
+      <q-icon class="q-pa-md" name="fas fa-dove" size="lg" color="primary" />
+
+      <q-list>
+        <q-item to="/" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="home" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+
+        <q-item to="/about" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="help" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold"
+            >About</q-item-section
+          >
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
